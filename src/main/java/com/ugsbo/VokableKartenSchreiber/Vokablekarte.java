1465ef8273d2816c5f769fb555d1033d514606ec
Christian Baltzer <christian@baltzer.de>
2019-07-02 22:24:22 +0200

    VokabelKartenschreiber Logik geschrieben

---
+++ src/main/java/com/ugsbo/VokableKartenSchreiber/Vokablekarte.java
@@ -0,0 +1,94 @@
+package com.ugsbo.VokableKartenSchreiber;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+class Vokabelkarte{
+	String Name;
+	String Frage;
+	String Antwort1;
+	String Antwort2;
+	String Antwort3;
+	String Antwort4;
+	
+	String Ergebnis;
+	
+    Scanner sc;
+    BufferedWriter bw;
+    
+    
+    
+	/**
+	 * @param sc
+	 * @param bw
+	 */
+	public Vokabelkarte() {
+		this.sc =  new Scanner(System.in);
+		
+		try {
+            this.bw = new BufferedWriter(new FileWriter("Data.txt", true));
+        } catch (Exception e) {
+            System.out.println("ne, wir nichts");
+        }
+	}
+	
+    /***
+     * 
+     * @param zwischenstück		fügt ein Zwischenstück zwischen den beiden Strings ein
+     * @param prüfen	Der zu prüfende String
+     * @param Rückgabe	Der String an den angehängt werden soll
+     * @return	Der fertige String
+     */
+	
+	
+	//-------------------------------------------------
+	//Private
+    private String NichtLeeralsohinzufügen(String prüfen, String Rückgabe, String Zwischenwert) {
+    	if (prüfen != "") {
+    		if(Rückgabe!="") {
+    			Rückgabe += Zwischenwert;
+    		}
+    		Rückgabe += prüfen;
+    	}
+    	return Rückgabe;
+    }
+    
+	//-------------------------------------------------
+    //Public
+    
+    /***
+     * Schreibt den Ergebnis String in eine Datei
+     */
+    public void schreiben() {
+    	String Text = this.Ergebnis;
+        System.out.println(Text);
+        try {
+            bw.write(Text);
+            bw.write("\n");
+            bw.flush();
+        } catch (Exception e) {
+            System.out.println("AHHHHHHHHH");
+        }
+        
+    }
+    
+    /***
+     * Erstellt aus den Eingetragenen Feldern den endgüligen String im gültigen Format
+     * Muster: Name/Frage/Antwort;Antwort
+     * @return gibt den formatierten String zurück
+     */
+    public String makeString(String Name,String Frage,String Antwort1,String Antwort2,String Antwort3,String Antwort4) {
+    	String Ergebnis = "";
+    	Ergebnis = NichtLeeralsohinzufügen(Ergebnis, Name, "");
+    	Ergebnis = NichtLeeralsohinzufügen(Ergebnis, Frage, "/");
+    	Ergebnis = NichtLeeralsohinzufügen(Ergebnis, Antwort1, "/");
+    	Ergebnis = NichtLeeralsohinzufügen(Ergebnis, Antwort2, ";");
+    	Ergebnis = NichtLeeralsohinzufügen(Ergebnis, Antwort3, ";");
+    	Ergebnis = NichtLeeralsohinzufügen(Ergebnis, Antwort4, ";");
+    	
+    	this.Ergebnis = Ergebnis;
+    	return Ergebnis;
+    }
+    
+}
