90e3239ec681f1e74b033f867b004cbd0a9f7d36
Christian Baltzer <christian@baltzer.de>
2019-07-05 21:41:38 +0200

    Logik eingefügt

--- src/main/java/com/ugsbo/IsMyComputerOn/IsMyComputerOn_App.java
+++ src/main/java/com/ugsbo/IsMyComputerOn/IsMyComputerOn_App.java
@@ -1,20 +1,30 @@
 package com.ugsbo.IsMyComputerOn;
 
+import java.util.concurrent.TimeUnit;
+
 public class IsMyComputerOn_App {
 
 	public static void main(String[] args) {
-		
-
+		while (true) {
+			System.out.print("Dein Rechner ist: ");
+			System.out.println((IstErAn()?"AN":"AUS"));
+			try {
+				TimeUnit.MINUTES.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	/***
 	 * 
-	 * @return 
+	 * @return true 	Da wir davon ausgehen, dass das Program nur ausgeführt werden kann wenn der Rechner läuft, geben wir immer true zurück. 
 	 */
 
  	public static boolean IstErAn() {
 	// TODO Auto-generated method stub
-	return false;
+	return true;
 	}
 
 }
