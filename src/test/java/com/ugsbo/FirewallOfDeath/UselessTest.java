c720e31b60d64235749ab67e4768f4a7c25e428a
Christian Baltzer <christian@baltzer.de>
2019-07-05 21:57:45 +0200

    Init

---
+++ src/test/java/com/ugsbo/FirewallOfDeath/UselessTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.ugsbo.FirewallOfDeath;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author christian
+ *	Tests für die Funktion FirewallOfDeath, welche sich schlecht testen lässt ._.
+ */
+public class UselessTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
