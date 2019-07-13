f4d2f14195419617f1350eb687e991ef6d062c20
elephant12 <caro.brueckmann@web.de>
2019-07-13 20:00:03 +0200

    neuer Test + Korrektur Test ergebnisTest
    neuer Test für die Methode fragen()
    und Test für die Methode ergebnis() wurde korrigiert

--- src/test/java/com/ugsbo/entscheider/JUnitEntscheiderErgebnisTest.java
+++ src/test/java/com/ugsbo/entscheider/JUnitEntscheiderErgebnisTest.java
@@ -15,18 +15,24 @@ public class JUnitEntscheiderErgebnisTest {
 		int a = Entscheider.skalaTest(11);
 		Assert.assertTrue(a >= 1 || a <= 10 );
 		
-		//fail("Not yet implemented");
 	}
 	
 	@Test
 	public void ergebnisTest() {
-		Assert.assertTrue("" == Entscheider.ergebnis(1,1,1,1,1,1,1,1));
+		String a = Entscheider.ergebnis(1,1,1,1,1,1,1,1);
+		Assert.assertTrue(a == "Ich kann doch nicht fuer dich entscheiden, dass musst du schon selber wissen." );
 	}
 	
 	@Test
 	public void pruefenTest() {
 		int a = Entscheider.pruefen(2);
 		Assert.assertTrue(0 == a || 1 == a);
+	}
+	
+	@Test
+	public void fragen() {
+		String a = Entscheider.fragen();
+		Assert.assertTrue(a == "" );
 	}
 
 }
