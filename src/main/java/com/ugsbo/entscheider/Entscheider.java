e35f176f6bdce6cc7747ccee346a97b0351de0a8
elephant12 <caro.brueckmann@web.de>
2019-07-13 20:44:38 +0200

    Implementierung der Methode fragen()
    hier werden 9 verschiedene Fragen gestellt, die Ergebnisse werden an die Methode ergebnis() weitergegeben welche sie asuwertet und wieder an die Methode fragen() zurückgibt - diese gibt das Ergebnis dann aus

--- src/main/java/com/ugsbo/entscheider/Entscheider.java
+++ src/main/java/com/ugsbo/entscheider/Entscheider.java
@@ -6,6 +6,7 @@ package com.ugsbo.entscheider;
 
 import java.util.Scanner;
 import java.util.*;
+
 /**
  * @author bruec
  *
@@ -16,53 +17,164 @@ public class Entscheider {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println(ergebnis(29,1,1,3,2,1,1,4));
-		
+		System.out.println(ergebnis(29, 1, 1, 3, 2, 1, 1, 4));
 
 	}
-	
+
 	public static String ergebnis(int a, int b, int c, int d, int e, int f, int g, int h) {
-		int erg = (((a + b + c) * d ) /(e + f + g)) - h;
+		int erg = (((a + b + c) * d) / (e + f + g)) - h;
 		String hilfe = "";
-		if(erg < 5)  hilfe = "Ich kann doch nicht fuer dich entscheiden, dass musst du schon selber wissen.";
-		if(erg >= 5 && erg < 15) hilfe = "Naja, dann geh halt nach Hause und ruh dich aus.";
-		if(erg >= 15) hilfe = "Jetzt wieder gehen? Dann bist du doch voellig umsonst aufgestanden. Geh einfach hin.";
-		
+		if (erg < 5)
+			hilfe = "Ich kann doch nicht fuer dich entscheiden, dass musst du schon selber wissen.";
+		if (erg >= 5 && erg < 15)
+			hilfe = "Naja, dann geh halt nach Hause und ruh dich aus.";
+		if (erg >= 15)
+			hilfe = "Jetzt wieder gehen? Dann bist du doch voellig umsonst aufgestanden. Geh einfach hin.";
+
 		return hilfe;
 	}
-	
+
 	public static String fragen() {
-		return "";
+		// Eingangsfrage
+		System.out.println(
+				"Du willst also wissen ob du in die Veranstaltung gehen sollst oder nicht? Gib 1 für Ja ein 0 für Nein.");
+		int a = getAnInteger();
+		if (a == 1 || a == 0) {
+			if (a == 1) {
+				System.out.println("Dann werde ich dir jetzt ein paar Fragen stellen");
+			} else {
+				return("Na dann halt nicht. Tschüssi.");
+			}
+
+		} else {
+			if (a == 1) {
+				System.out.println("Dann werde ich dir jetzt ein paar Fragen stellen");
+			} else {
+				return ("Na dann halt nicht. Tschüssi.") ;
+			}
+
+		}
+		// zweite Frage
+		System.out.println("Wie alt bist du?");
+		int alter = getAnInteger();
+		if (alter > 0) {
+			System.out.println(alter);
+		} else {
+			while (alter <= 0) {
+				System.out.println("Versuches es nochmal. Du musst mindestens 1 sein.");
+				alter = getAnInteger();
+			}
+		}
+
+		// dritte Frage
+		System.out.println("Auf einer Skala von 1 bis 10 wie motiviert bist du?");
+		int mot = getAnInteger();
+		if (mot >= 1 && mot <= 10) {
+			System.out.println(mot);
+		} else {
+			skalaTest(mot);
+			System.out.println(mot);
+		}
+
+		// vierte Frage
+		System.out.println("Hast du gefrühstückt? Bei Ja bitte 1 und bei Nein bitte 0");
+		int fruehstueck = getAnInteger();
+		if (fruehstueck == 1 || fruehstueck == 0) {
+			System.out.println(fruehstueck);
+		} else {
+			pruefen(fruehstueck);
+			System.out.println(fruehstueck);
+		}
+
+		// fuenfte Frage
+		System.out.println("Hast du jemals ein Harry Potterbuch gelesen? Bei Ja bitte 1 und bei Nein bitte 0");
+		int harry = getAnInteger();
+		if (harry == 1 || harry == 0) {
+			System.out.println(harry);
+		} else {
+			pruefen(harry);
+			System.out.println(harry);
+		}
+
+		// sechste Frage
+		System.out.println("Wie viele Äpfel hast du heute schon gegessen?");
+		int apfel = getAnInteger();
+		if (apfel > 0) {
+			System.out.println(apfel);
+		} else {
+			while (apfel <= 0) {
+				System.out.println("Versuches es nochmal. Die Zahl muss positiv sein.");
+				apfel = getAnInteger();
+			}
+		}
+
+		// siebte Frage
+		System.out.println("Wie viele Veranstaltungen hattest du heute schon?");
+		int anzahl = getAnInteger();
+		if (anzahl > 0) {
+			System.out.println(anzahl);
+		} else {
+			while (anzahl <= 0) {
+				System.out.println("Versuches es nochmal. Die Zahl muss positiv sein.");
+				anzahl = getAnInteger();
+			}
+		}
+
+		// achte Frage
+		System.out.println("Was würdest du statt der Vorlesung machen? Lernen? Bei Ja bitte 1 und bei Nein bitte 0");
+		int lernen = getAnInteger();
+		if (lernen == 1 || lernen == 0) {
+			System.out.println(lernen);
+		} else {
+			pruefen(lernen);
+			System.out.println(lernen);
+		}
+		if (lernen == 1)
+			System.out.println("Wenn du das sagst, aber lueg dich doch bitte nicht selbst an.");
+
+		// neunte Frage
+		System.out.println("Wuerdest du dir ein gelbes Auto kaufen? Bei Ja bitte 1 und bei Nein bitte 0");
+		int gelb = getAnInteger();
+		if (gelb == 1 || gelb == 0) {
+			System.out.println(gelb);
+		} else {
+			pruefen(gelb);
+			System.out.println(gelb);
+		}
+
+		// Auswertung
+
+		String antwort = ergebnis(alter, lernen, gelb, apfel, mot, harry, fruehstueck, anzahl);
+		return antwort;
+
 	}
-	
-	public static int skalaTest(int answer){
+
+	public static int skalaTest(int answer) {
 		System.out.println("Try again. Nur Werte zwischen 1 und 10 sind erlaubt.");
 		int b = getAnInteger();
-		if(b >= 1 && b <= 10) {
+		if (b >= 1 && b <= 10) {
 			return b;
 		}
 		skalaTest(b);
 		return -1;
 	}
-	
+
 	public static int getAnInteger() {
 		Scanner in = new Scanner(System.in);
-		while (true){
-			try{
+		while (true) {
+			try {
 				return in.nextInt();
-			}
-			catch (InputMismatchException e){
+			} catch (InputMismatchException e) {
 				in.next();
 				System.out.println("Das ist kein Integer. Try again.");
 			}
 		}
 	}
-	
-	
+
 	public static int pruefen(int answer) {
 		System.out.println("Try again. Nur 1 und 0 sind erlaubt.");
 		int b = getAnInteger();
-		if(b == 1 || b == 0) {
+		if (b == 1 || b == 0) {
 			return b;
 		}
 		pruefen(b);
