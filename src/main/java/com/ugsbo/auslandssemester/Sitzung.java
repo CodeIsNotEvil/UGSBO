137886491cad41d89599d4d3c4b1dd01e8a5b69b
elephant12 <caro.brueckmann@web.de>
2019-07-14 23:38:26 +0200

    Hinzufügen von Variablen und Konstruktor

--- src/main/java/com/ugsbo/auslandssemester/Sitzung.java
+++ src/main/java/com/ugsbo/auslandssemester/Sitzung.java
@@ -1,5 +1,18 @@
 package com.ugsbo.auslandssemester;
 
 public class Sitzung {
+	private String name;
+	private String vorname;
+	private int inMonaten;
+	private String destination;
+
+
+	// Konstruktor mit Übergabewerten
+	public Sitzung(String nachname, String vor, int monate, String ziel) {
+		this.name = nachname;
+		this.vorname = vor;
+		this.inMonaten = monate;
+		this.destination = ziel;
+	}
 
 }
