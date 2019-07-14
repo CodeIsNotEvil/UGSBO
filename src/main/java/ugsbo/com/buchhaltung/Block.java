a38ee183ad8a16388afaaa2d44b2b08bfd308cd6
Christian Baltzer <christian@baltzer.de>
2019-07-14 16:20:51 +0200

    Block hinzugefügt

--- src/main/java/ugsbo/com/buchhaltung/Block.java
+++ src/main/java/ugsbo/com/buchhaltung/Block.java
@@ -1,14 +1,24 @@
 package ugsbo.com.buchhaltung;
 
+import java.security.MessageDigest;
+
 public class Block {
 
   int data;
   int kontostand;
   Block vorher;
   
+  String ownHash;
+  String previousHash;
   
   public Block(int Data) {
     data = Data;
     vorher = null;
+    ownHash = createNewHash();
+    previousHash = null;
+  }
+
+  private String createNewHash() {
+    return null;
   }
 }
