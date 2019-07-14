6b63ca7e4ba12e7bb57c152299d94e4e69c86b5f
elephant12 <caro.brueckmann@web.de>
2019-07-14 23:45:51 +0200

    nue Testfälle zur Methode kategorieDestination
    jetzt sollten alle möglichen Eingaben abgedeckt sein

--- src/test/java/com/ugsbo/auslandssemester/JUnitTestAusslandssemesterSitzung.java
+++ src/test/java/com/ugsbo/auslandssemester/JUnitTestAusslandssemesterSitzung.java
@@ -12,16 +12,54 @@ import com.ugsbo.entscheider.Entscheider;
 
 public class JUnitTestAusslandssemesterSitzung {
 
-	
-
-	//USA als Destination
+	// USA als Destination
 	@Test
 	public void KategorieDestinationUSA() {
 		Sitzung test = new Sitzung("Mustermann", "Max", 2, "USA");
 		String erwartet = "USA";
 		String b = test.kategorieDestination();
-		assertEquals("Antwortstring sollte USA sein",erwartet,b);
-		
+		assertEquals("Antwortstring sollte USA sein", erwartet, b);
+
+	}
+
+	// Asien als Destination
+	@Test
+	public void KategorieDestinationAsien() {
+		Sitzung test = new Sitzung("Mustermann", "Max", 2, "Asien");
+		String erwartet = "Asien";
+		String b = test.kategorieDestination();
+		assertEquals("Antwortstring sollte Asien sein", erwartet, b);
+
+	}
+
+	// Neuseeland als Destination
+	@Test
+	public void KategorieDestinationNeuseeland() {
+		Sitzung test = new Sitzung("Mustermann", "Max", 2, "Neuseeland");
+		String erwartet = "Asien";
+		String b = test.kategorieDestination();
+		assertEquals("Antwortstring sollte Asien sein", erwartet, b);
+
+	}
+
+	// Australien als Destination
+	@Test
+	public void KategorieDestinationAustralien() {
+		Sitzung test = new Sitzung("Mustermann", "Max", 2, "Australien");
+		String erwartet = "Asien";
+		String b = test.kategorieDestination();
+		assertEquals("Antwortstring sollte Asien sein", erwartet, b);
+
+	}
+
+	// Europa als Destination
+	@Test
+	public void KategorieDestinationEuropa() {
+		Sitzung test = new Sitzung("Mustermann", "Max", 2, "Europa");
+		String erwartet = "Europa";
+		String b = test.kategorieDestination();
+		assertEquals("Antwortstring sollte Europa sein", erwartet, b);
+
 	}
 
 }
