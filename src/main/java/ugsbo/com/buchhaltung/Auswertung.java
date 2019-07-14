a91ee44386a51f4964ebd247b818416bcf5cdf13
Christian Baltzer <christian@baltzer.de>
2019-07-14 16:55:05 +0200

    Auswertung-class fertig

--- src/main/java/ugsbo/com/buchhaltung/Auswertung.java
+++ src/main/java/ugsbo/com/buchhaltung/Auswertung.java
@@ -4,7 +4,12 @@ public class Auswertung {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-
+    Blockchain Work = new Blockchain();
+    Work.add(50);
+    Work.add(-20);
+    
+    System.out.println(Work.kontostand());
+    System.out.print(Work.toString());
   }
 
 }
