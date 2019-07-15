95d3d1cb28f8a158c892ef10d4144447560843ff
Lukas Reichwein <codeisnotevil@gmail.com>
2019-07-15 09:38:35 +0200

    linked calculating the Determinants of the input Matrices to their Buttons

--- src/main/java/com/ugsbo/matrixcalc/MatrixCalcController.java
+++ src/main/java/com/ugsbo/matrixcalc/MatrixCalcController.java
@@ -9,7 +9,7 @@ public class MatrixCalcController {
 
     // Hier werden die fx:id Attribute verknuepft.
     @FXML
-    private Button multiplyButton, addButton, swapInputButton, substractButton, transposeButton;
+    private Button multiplyButton, addButton, DetAButton, DetBButton, substractButton, transposeButton;
     @FXML
     private Text errorText, outputText;
     @FXML
@@ -100,6 +100,40 @@ public class MatrixCalcController {
                 double[][] result = math.matrixSubstraction(matrixA, matrixB);
 
                 String DisplayableString = util.outputMatrixToOutputText(result);
+                
+                outputText.setText(DisplayableString);
+                outputText.setTextAlignment(TextAlignment.CENTER);
+            }
+        });
+
+        DetAButton.setOnMouseClicked((event) -> {
+            MatrixCalcMath math = new MatrixCalcMath();
+            MatrixCalcIOUtils util = new MatrixCalcIOUtils();
+
+            String stringMatrixA = matrixATextArea.getText();
+            if (util.checkInput(stringMatrixA)) {
+
+                double[][] matrixA = util.stringToMatrix(stringMatrixA);
+                double result = math.calcDeterminat(matrixA);
+
+                String DisplayableString = Double.toString(result);
+                
+                outputText.setText(DisplayableString);
+                outputText.setTextAlignment(TextAlignment.CENTER);
+            }
+        });
+
+        DetBButton.setOnMouseClicked((event) -> {
+            MatrixCalcMath math = new MatrixCalcMath();
+            MatrixCalcIOUtils util = new MatrixCalcIOUtils();
+
+            String stringMatrixB = matrixBTextArea.getText();
+            if (util.checkInput(stringMatrixB)) {
+
+                double[][] matrixB = util.stringToMatrix(stringMatrixB);
+                double result = math.calcDeterminat(matrixB);
+
+                String DisplayableString = Double.toString(result);
                 
                 outputText.setText(DisplayableString);
                 outputText.setTextAlignment(TextAlignment.CENTER);
