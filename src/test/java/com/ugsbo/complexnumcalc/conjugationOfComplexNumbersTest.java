3cf9fc1ba5a1fff9e8e043065516897de8f3c519
Lukas Reichwein <lukas.reichwein@informatik.hs-fulda.de>
2019-07-16 19:29:59 +0200

    changed tests for the conjugation of conplex numbers to use hamcrest asserrThat

--- src/test/java/com/ugsbo/complexnumcalc/conjugationOfComplexNumbersTest.java
+++ src/test/java/com/ugsbo/complexnumcalc/conjugationOfComplexNumbersTest.java
@@ -1,6 +1,7 @@
 package com.ugsbo.complexnumcalc;
 
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
 
@@ -15,8 +16,18 @@ public class conjugationOfComplexNumbersTest {
         
         ComplexNumber actual = complexNumber.conjugationOf();
 
-        assertTrue("TheConjugatedComplexNumberOfAComplexNumberWithOnlyARealPartIsTheRealPart", expected.equals(actual));
-        //TODO change equals to equalTo
-        //assertThat("TheConjugatedComplexNumberOfAComplexNumberWithOnlyARealPartIsTheRealPart", expected, equalsTo(actual));
+        assertThat("The conjugated complex Number of a complex number with only a real part is the real part", expected, equalTo(actual));
+    }
+
+    @Test
+    public void TheConjugatedComplexNumberOfAComplexNumberShouldBeTheComplexNumberWithSwapedSignImaginaryPart_ButItIsNot() { 
+        Double realPart = Double.valueOf(4);
+        Double imaginaryPart = Double.valueOf(5);
+        ComplexNumber complexNumber = new ComplexNumber(realPart, imaginaryPart);
+        ComplexNumber expected = new ComplexNumber(realPart, -imaginaryPart);
+        
+        ComplexNumber actual = complexNumber.conjugationOf();
+
+        assertThat("The conjugated complex number of a complex number is the complex number with swaped sign in the imaginary part", expected, equalTo(actual));
     }
 }
