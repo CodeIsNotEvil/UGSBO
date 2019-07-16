c1c08558ca92a9491b38a2df0b56b26da1510b43
Lukas Reichwein <lukas.reichwein@informatik.hs-fulda.de>
2019-07-16 19:36:21 +0200

    changed SubstrationTests to use hamcrest assertThat instead of assertTrue

--- src/test/java/com/ugsbo/complexnumcalc/SubstractComplexNumbersTest.java
+++ src/test/java/com/ugsbo/complexnumcalc/SubstractComplexNumbersTest.java
@@ -1,6 +1,7 @@
 package com.ugsbo.complexnumcalc;
 
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
 
@@ -14,7 +15,7 @@ public class SubstractComplexNumbersTest {
 
         ComplexNumber difference = minuend.substract(subtrahend);
 
-        assertTrue("The difference is not as expected", difference.equals(expected));
+        assertThat("The difference is not as expected", difference, equalTo(expected));
     }
 
     @Test
@@ -25,6 +26,6 @@ public class SubstractComplexNumbersTest {
 
         ComplexNumber difference = minuend.substract(subtrahend);
 
-        assertTrue("The difference is not as expected", difference.equals(expected));
+        assertThat("The difference is not as expected", difference, equalTo(expected));
     }
 }
