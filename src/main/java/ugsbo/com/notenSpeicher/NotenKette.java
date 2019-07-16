aeb4fa731e35c74c673b2c91db481e3566b6ff78
Christian Baltzer <christian@baltzer.de>
2019-07-16 16:16:38 +0200

    Tests geschrieben

--- src/main/java/ugsbo/com/notenSpeicher/NotenKette.java
+++ src/main/java/ugsbo/com/notenSpeicher/NotenKette.java
@@ -6,4 +6,14 @@ public class NotenKette {
     // TODO Auto-generated constructor stub
   }
 
+  public int durchschnitt() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public void add(String eingabeFach, int eingabeNote) {
+    // TODO Auto-generated method stub
+    
+  }
+
 }
--- src/test/java/com/ugsbo/notenSpeicher/Test_NotenChain.java
+++ src/test/java/com/ugsbo/notenSpeicher/Test_NotenChain.java
@@ -3,15 +3,61 @@ package com.ugsbo.notenSpeicher;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+import ugsbo.com.notenSpeicher.NotenKette;
 
 public class Test_NotenChain {
 
+  public NotenKette Workingobjekt;
+  
   @Before
-  public void setUp() throws Exception {}
-
-  @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void setUp() throws Exception {
+    Workingobjekt = new NotenKette();
   }
 
+  @Test
+  public void hinzufügen() {
+    String eingabeFach = "Mathe";
+    int eingabeNote = 2;
+    
+    int erwartet = 2;
+    int ergebnis;
+    
+    Workingobjekt.add(eingabeFach, eingabeNote);
+    ergebnis = Workingobjekt.durchschnitt();
+    
+    assertEquals("Durchschnitt ist korrekt", erwartet, ergebnis);
+  }
+  
+  @Test
+  public void hinzufügenMehrAlsEinmal() {
+    String eingabeFach = "Mathe";
+    int eingabeNote = 2;
+    int ergebnis;
+    int erwartet = 2;
+    
+    Workingobjekt.add(eingabeFach, eingabeNote);
+    Workingobjekt.add(eingabeFach, eingabeNote);
+    Workingobjekt.add(eingabeFach, eingabeNote);
+    
+    ergebnis = Workingobjekt.durchschnitt();
+    
+    assertEquals("eingabe und Ergebnis sind gleich", erwartet, ergebnis);
+  }
+  
+  @Test
+  public void hinzufügenistNull() {
+    String eingabeFach = "Mathe";
+    int eingabeNote = 2;
+    int eingabeNotezwei = 0;
+    int ergebnis;
+    int erwartet = 2;
+    
+    Workingobjekt.add(eingabeFach, eingabeNote);
+    Workingobjekt.add(eingabeFach, eingabeNotezwei);
+    
+    ergebnis = Workingobjekt.durchschnitt();
+    
+    assertEquals("Durchscnitt hat sich nicht verändert", erwartet, ergebnis);
+  }
+  
 }
