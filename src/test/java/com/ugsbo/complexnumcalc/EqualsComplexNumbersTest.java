08396a8d2c79aca4aa9d274773207a710fc9ad81
Lukas Reichwein <lukas.reichwein@informatik.hs-fulda.de>
2019-07-16 19:53:06 +0200

    fixed a Typo in the EqualsComplexNumversTest class to EqualsComplexNumbersTest class

--- src/test/java/com/ugsbo/complexnumcalc/EqualsComplexNumbersTest.java
+++ src/test/java/com/ugsbo/complexnumcalc/EqualsComplexNumbersTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class EqualsComplexNumversTest {
+public class EqualsComplexNumbersTest {
 
     @Test
     public void TwoEqualNumbersWithOnlyRealPart_AreNotDetectedAsEqual() {
